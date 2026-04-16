b7fb179c6a5d5358e3c678b96146c10a71f3ecc6
RETHESSHED <edrethessh@gmail.com>
2026-04-16 15:58:42 +0530

    Update B10_Library_Management_using_Collections.java

--- Springboot/Java/Assignment/src/B10_Library_Management_using_Collections.java
+++ Springboot/Java/Assignment/src/B10_Library_Management_using_Collections.java
@@ -9,25 +9,93 @@ public class B10_Library_Management_using_Collections {
 		list1.add(new B10_Library(2,"The mong","Ajay"));
 		list1.add(new B10_Library(3,"The monkey","Jaff \n"));
 		
-		list1.forEach(System.out::println);
+		Scanner a =new Scanner(System.in); 
+		boolean sh = true;
 		
-		String search = "The monkey";
+		while(sh){
+		System.out.println("Library Management :\n");
+		System.out.println("1. Add new book");
+		System.out.println("2. Display all books");
+		System.out.println("3. Search book by title");
+		System.out.println("4. Remove book by ID");
+		System.out.println("5. Sort books by title ");
+		System.out.println("6. close \n");
 		
-        for (B10_Library b : list1) {
-            if (b.getTitle().equalsIgnoreCase(search)) {
-                System.out.println("\nFound: " + b);
-            }
-        }
-        
-        int Remove = 2;
-        list1.removeIf(B10_Library -> B10_Library.getBookId() == Remove);
-        
-        list1.forEach(System.out::println);
-        
-        //list1.sort((e1,e2) -> e1.getTitle().compareTo(e2.getTitle()));
-        list1.sort(Comparator.comparing(e -> e.getTitle()));
-        
-        list1.forEach(System.out::print);
+		System.out.println("\n Enter the option: ");
+		int s = a.nextInt();
+		switch(s) {
+		case(1):
+			System.out.println("\n Enter Id: ");
+			int a1=a.nextInt();
+			a.nextLine(); 
+			System.out.println("\n Enter Title: ");
+			String a2=a.nextLine();
+			System.out.println("\n Enter Author: ");
+			String a3=a.nextLine();
+			list1.add(new B10_Library(a1,a2,a3));
+			break;
+		
+		case(2):
+			System.out.println("\nList of Books: ");
+			list1.forEach(System.out::println);
+			break;
+		
+		case(3):
+			a.nextLine(); 
+			System.out.println("\n Enter the title: ");
+			String search = a.nextLine();
+	        for (B10_Library b : list1) {
+	            if (b.getTitle().equalsIgnoreCase(search)) {
+	                System.out.println("\nFound: " + b);
+	            }
+	        }
+	        break;
+	     
+		case(4):
+			System.out.println("\n Enter the Id: ");
+			int remove = a.nextInt();
+			list1.removeIf(B10_Library -> B10_Library.getBookId() == remove);
+			break;
+		
+		case(5):
+			System.out.println("\n Sorted book by title: ");
+			list1.sort(Comparator.comparing(e -> e.getTitle()));
+			break;
+		
+		case(6):
+			sh = false;
+			break;
+			
+		}
+		
+		
+		
+		
+		
+		
+		}
+		
+		
+		
+//		list1.forEach(System.out::println);
+//		
+//		String search = "The monkey";
+//		
+//        for (B10_Library b : list1) {
+//            if (b.getTitle().equalsIgnoreCase(search)) {
+//                System.out.println("\nFound: " + b);
+//            }
+//        }
+//        
+//        int Remove = 2;
+//        list1.removeIf(B10_Library -> B10_Library.getBookId() == Remove);
+//        
+//        list1.forEach(System.out::println);
+//        
+//        //list1.sort((e1,e2) -> e1.getTitle().compareTo(e2.getTitle()));
+//        list1.sort(Comparator.comparing(e -> e.getTitle()));
+//        
+//        list1.forEach(System.out::print);
 	}
 	
 
